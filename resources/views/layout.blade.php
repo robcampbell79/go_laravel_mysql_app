2b9fcdc6f864bbaddcd1bd62eff6b889a56e8ab0
Robert <robcampbell@ucmo.edu>
2019-03-08 16:56:27 -0600

    Add Go to title

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel Mysql Test Application</title>
+    <title>Go Laravel Mysql Test Application</title>
     <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css" />
   </head>
   <body>
